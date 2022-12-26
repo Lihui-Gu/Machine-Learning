84f0b0d3f0166a07b5c28460772279c51ea0ddac
lihui.gu <lihui.gu@phigent.ai>
2022-12-26 18:34:39 +0800

    [feat]complete resnet homework py torch

---
+++ week4/resnet/train.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import argparse
+from resnet import ResNet18
+import os
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--outf', default='./model/', help='folder to output images and model checkpoints') #输出结果保存路径
+args = parser.parse_args()
+
+# 超参数设置
+EPOCH = 135   #遍历数据集次数
+pre_epoch = 0  # 定义已经遍历数据集的次数
+BATCH_SIZE = 128      #批处理尺寸(batch_size)
+LR = 0.01        #学习率
+
+# 准备数据集并预处理
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),  #先四周填充0，在吧图像随机裁剪成32*32
+    transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), #R,G,B每层的归一化用到的均值和方差
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train) 
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)   
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+net = ResNet18().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4) 
+# 训练
+if __name__ == "__main__":
+    if not os.path.exists(args.outf):
+        os.makedirs(args.outf)
+    best_acc = 85
+    print("Start Training, Resnet-18!")  #
+    with open("acc.txt", "w") as f:
+        with open("log.txt", "w")as f2:
+            for epoch in range(pre_epoch, EPOCH):
+                print('\nEpoch: %d' % (epoch + 1))
+                net.train()
+                sum_loss = 0.0
+                correct = 0.0
+                total = 0.0
+                for i, data in enumerate(trainloader, 0):
+                    # 准备数据
+                    length = len(trainloader)
+                    inputs, labels = data
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    optimizer.zero_grad()
+
+                    # forward + backward
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
+
+                    # 每训练1个batch打印一次loss和准确率
+                    sum_loss += loss.item()
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += predicted.eq(labels.data).cpu().sum()
+                    print('[epoch:%d, iter:%d] Loss: %.03f | Acc: %.3f%% '
+                          % (epoch + 1, (i + 1 + epoch * length), sum_loss / (i + 1), 100. * correct / total))
+                    f2.write('%03d  %05d |Loss: %.03f | Acc: %.3f%% '
+                          % (epoch + 1, (i + 1 + epoch * length), sum_loss / (i + 1), 100. * correct / total))
+                    f2.write('\n')
+                    f2.flush()
+
+                print("Waiting Test!")
+                with torch.no_grad():
+                    correct = 0
+                    total = 0
+                    for data in testloader:
+                        net.eval()
+                        images, labels = data
+                        images, labels = images.to(device), labels.to(device)
+                        outputs = net(images)
+                        _, predicted = torch.max(outputs.data, 1)
+                        total += labels.size(0)
+                        correct += (predicted == labels).sum()
+                    print('测试分类准确率为：%.3f%%' % (100 * correct / total))
+                    acc = 100. * correct / total
+                    print('Saving model......')
+                    torch.save(net.state_dict(), '%s/net_%03d.pth' % (args.outf, epoch + 1))
+                    f.write("EPOCH=%03d,Accuracy= %.3f%%" % (epoch + 1, acc))
+                    f.write('\n')
+                    f.flush()
+                    if acc > best_acc:
+                        f3 = open("best_acc.txt", "w")
+                        f3.write("EPOCH=%d,best_acc= %.3f%%" % (epoch + 1, acc))
+                        f3.close()
+                        best_acc = acc
+            print("Training Finished, TotalEPOCH=%d" % EPOCH)
+
+
+
